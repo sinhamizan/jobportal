dbd3a4dc2d7cc464defb2762e10376aa3ac4c926
Mizan <mizan522151@gmail.com>
2019-12-01 00:10:19 +0600

    category done

--- recruiterapp/urls.py
+++ recruiterapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recruiter-register/', views.recruiterRegister, name="recruiter_register"),
 ]
--- recruiterapp/views.py
+++ recruiterapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
+
+def home(request):
+    cat = Category.objects.all()
+    context = {
+        'cat':cat,
+    }
+    return render(request, 'home.html', context)
+
+def recruiterRegister(request):
+
+    return render(request, 'recruiter_register.html')
+
+
+
